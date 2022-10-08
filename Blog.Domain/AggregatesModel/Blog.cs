f84f7c5651f67748153c3d037c3790494ebb4031
MajidEbadiParsa <majid.e@cymax.com>
2022-10-08 13:17:08 +0200

    Tests' Blog Domain

---
+++ Blog.Domain/AggregatesModel/Blog.cs
@@ -0,0 +1,27 @@
+﻿namespace Blog.Domain.AggregatesModel
+{
+    public class Blog
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        private Blog(int id, string name, string description)
+        {
+            if (id < 1)
+                throw new ArgumentException("The Id field must be greater than zero.");
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("The Name field must not be empty");
+
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public static Blog CreateBlog(int id, string name, string description)
+        {
+            return new Blog(id, name, description);
+        }
+    }
+}
---
+++ Blog.Test/Domain/BlogDomainTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Blog.Test.Domain
+{
+    public class BlogDomainTest
+    {
+        private const int VALID_BLOG_ID = 1;
+        private const string VALID_BLOG_NAME = "Tech";
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public async Task CreateBlog_InvalidBlogId_ShouldBeThrowAnException(int id)
+        {
+            Assert.Throws<ArgumentException>(() => Blog.Domain.AggregatesModel.Blog.CreateBlog(id, VALID_BLOG_NAME, string.Empty));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        public async Task CreateBlog_InvalidBlogName_ShouldBeThrowAnException(string name)
+        {
+            Assert.Throws<ArgumentException>(() => Blog.Domain.AggregatesModel.Blog.CreateBlog(VALID_BLOG_ID, name, string.Empty));
+        }
+    }
+}
