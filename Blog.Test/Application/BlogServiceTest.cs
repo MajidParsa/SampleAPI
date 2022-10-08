39a8add08b7942f12ac6490065bcdb0ebdc5d57c
MajidEbadiParsa <majid.e@cymax.com>
2022-10-08 12:48:15 +0200

    Add BlogServiceTest

---
+++ Blog.Test/Application/BlogServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Application;
+using Blog.Application.Services;
+using Blog.Infrastructure.Repositories.Blog;
+using Moq;
+using NUnit.Framework;
+
+namespace Blog.Test.Application
+{
+    public class BlogServiceTest
+    {
+        private IBlogService _blogService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockBlogRepository = new Mock<BlogRepository>();
+            var mockBlogService = new Mock<BlogService>(mockBlogRepository.Object);
+
+            _blogService = mockBlogService.Object;
+        }
+    }
+}
